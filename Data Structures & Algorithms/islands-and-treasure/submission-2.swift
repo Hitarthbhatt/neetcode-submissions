ce3ecee4cff40803fa8e3e17e1dd53c99695cfa9
HItarth Bhatt <41588525+Hitarthbhatt@users.noreply.github.com>
2026-06-03 13:58:19 +0530

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.swift
@@ -0,0 +1,48 @@
+class Solution {
+    let MAX = 2147483647
+    var row = 0
+    var col = 0
+    func islandsAndTreasure(_ grid: inout [[Int]]) {
+        row = grid.count
+        col = grid[0].count
+        var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: col), count: row)
+
+        for i in 0..<row {
+            for j in 0..<col {
+                if grid[i][j] == 0 {
+                    bfs(&grid, i, j, &visited, 0)
+                }
+            }
+        }
+    }
+
+    func bfs(_ grid: inout [[Int]], _ i: Int, _ j: Int, _ visited: inout [[Bool]], _ level: Int) {
+        if i < 0 || j < 0 || i >= row || j >= col { return }
+        if grid[i][j] == -1 || (grid[i][j] == 0 && level != 0) {return }
+        if visited[i][j] { return }
+
+        if level != 0 {
+            if grid[i][j] < level {
+                return
+            } else {
+                grid[i][j] = min(level, grid[i][j])
+            }
+        }
+        visited[i][j] = true
+
+        // right
+        bfs(&grid, i, j+1, &visited, level + 1)
+
+        // down
+        bfs(&grid, i+1, j, &visited, level + 1)
+
+        // up
+        bfs(&grid, i-1, j, &visited, level + 1)
+
+        // left
+        bfs(&grid, i, j-1, &visited, level + 1)
+        
+        visited[i][j] = false
+    }
+
+}
