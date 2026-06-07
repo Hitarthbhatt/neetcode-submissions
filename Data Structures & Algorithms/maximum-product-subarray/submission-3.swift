b945ccbf40cffdb1aee21689cd1948e7113da27b
HItarth Bhatt <41588525+Hitarthbhatt@users.noreply.github.com>
2026-06-07 19:28:24 +0530

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func maxProduct(_ nums: [Int]) -> Int {
+        var res = nums[0]
+        var curMin = 1, curMax = 1
+
+        for num in nums {
+            let tmp = curMax * num
+            curMax = max(tmp, num * curMin, num)
+            curMin = min(tmp, num * curMin, num)
+            res = max(res, curMax)
+        }
+        return res
+    }
+}
